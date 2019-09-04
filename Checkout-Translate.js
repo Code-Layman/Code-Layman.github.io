466c12cd11da0593748c3b473c506d6d31f19f21
Code-Layman <54162370+Code-Layman@users.noreply.github.com>
2019-09-04 23:43:54 +0800

    Update Checkout-Translate.js

--- Checkout-Translate.js
+++ Checkout-Translate.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
 $("#cc-checkout-steps li span").attr("class","trn");	
 $("h3").attr("class","trn");
+$("label.j-checkout__product-single-price").attr("class","trn");
+$("label.j-checkout__product-amount").attr("class","trn");
+$("label.j-checkout__product-price").attr("class","trn");
+$("label.j-checkout__products-subtotal-label").attr("class","trn");
+$("label.j-checkout__shipping-costs-label").attr("class","trn");	
+$("label.j-checkout__products-total-label").attr("class","trn");	
+$("label.j-checkout__customer-order-note__label").attr("class","trn");	
 
+function toEnglish(){
+$("#cc-checkout-submit-0").attr("Value","Next step");	
+}
+	
+function toGerman(){
+$("#cc-checkout-submit-0").attr("Value","Nächster Schritt");		
+}		
 		
 var local = {
         "Warenkorb":{en:"Shipping cart"},
@@ -12,15 +26,12 @@ var local = {
 	"Anzahl":{en:"Amount"},
 	"Preis":{en:"Price"},
 	"Zwischensumme":{en:"Subtotal"},
-	"Versandkosten":{en:"Shipping costs"},
-	"Gesamtsumme":{en:"Total"},
+	"Versandkosten*<small></small>:":{en:"Shipping costs*:"},
+	" Gesamtsumme<small></small>:":{en:" Total:"},
 	"Anmerkung zur Bestellung":{en:"Customer order note"},
 	"Herr":{en:"Mr"},
 	"Frau": {en:"Mrs"},
-	
-	
- 
-};	
+	};	
 
 var translator = $('body').translate({
     lang: "de", 
@@ -32,9 +43,11 @@ var country=$("#TranslateCheckout").children('option:selected').val()
         
 if(country == "en") {
 translator.lang("en")
+toEnglish()
 }
 else if(country == "de") {
 translator.lang("de")
+toGerman()	
 }
 });	
 });
