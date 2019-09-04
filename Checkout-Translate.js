ffae7e2f14f68b9f94ce96f94ece78533b73d7ae
Code-Layman <54162370+Code-Layman@users.noreply.github.com>
2019-09-04 23:04:57 +0800

    Update Checkout-Translate.js

--- Checkout-Translate.js
+++ Checkout-Translate.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
 $("#cc-checkout-steps li span").attr("class","trn");	
-$("h3").attr("class","trn");	
+$("h3").attr("class","trn");
+$("label").attr("class","trn");	
 		
 var local = {
-    "Warenkorb":{en:"Shipping cart"},
-    "Adresse angeben":{en:"Shipping address"},
-    "Bezahlen":{en:"Payment"},
-    "Bestätigen":{en:"Confirm"},
-    "Herr":{en:"Mr"},
-    "Frau": {en:"Mrs"},
+        "Warenkorb":{en:"Shipping cart"},
+        "Adresse angeben":{en:"Shipping address"},
+        "Bezahlen":{en:"Payment"},
+        "Bestätigen":{en:"Confirm"},
+        "Einzelpreis":{en:"Single price"},
+	"Anzahl":{en:"Amount"},
+	"Preis":{en:"Price"},
+	"Zwischensumme":{en:"Subtotal"},
+	"Versandkosten":{en:"Shipping costs"},
+	"Gesamtsumme":{en:"Total"},
+	"Anmerkung zur Bestellung":{en:"Customer order note"},
+	"Herr":{en:"Mr"},
+	"Frau": {en:"Mrs"},
+	
+	
+ 
 };	
 
 var translator = $('body').translate({
