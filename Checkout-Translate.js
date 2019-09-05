5fc59f2320cb388c63ae5804b9e4a6e61adaa6c0
Code-Layman <54162370+Code-Layman@users.noreply.github.com>
2019-09-05 22:54:42 +0800

    Update Checkout-Translate.js

--- Checkout-Translate.js
+++ Checkout-Translate.js
@@ -3,14 +3,22 @@ var webpage=window.location.href
 var reg = RegExp(/checkout/)
 if(webpage.match(reg)){ 	
 $("#cc-checkout-steps li span").attr("class","trn");	
-$("h3").attr("class","trn");
+$("#cc-checkout-paymentmethod-form h3").attr("class","trn");
 $("label.j-checkout__product-single-price").attr("class","j-checkout__product-single-price trn");
 $("label.j-checkout__product-amount").attr("class","j-checkout__product-amount trn");
 $("label.j-checkout__product-price").attr("class","j-checkout__product-price trn");
 $("label.j-checkout__products-subtotal-label").attr("class","trn");
 $("label.j-checkout__shipping-costs-label").attr("class","trn");	
 $("label.j-checkout__products-total-label").attr("class","trn");	
-$("label.j-checkout__customer-order-note__label").attr("class","trn");		
+$("label.j-checkout__customer-order-note__label").attr("class","trn");	
+	
+$("#cc-checkout-address-head-same").attr("class","trn");
+$("#cc-checkout-address-head-differs").attr("class","trn");	
+$("#option").attr("class","trn");	
+$(".j-checkout__input-label j-checkout__addressform-mandatory").attr("class","j-checkout__input-label j-checkout__addressform-mandatory trn");		
+$(".j-checkout__input-label ").attr("class","j-checkout__input-label trn");
+$(".cc-checkout-adressform-mandatory j-checkout__input-label optional").attr("class","cc-checkout-adressform-mandatory j-checkout__input-label optional trn");		
+$("#cc-checkout-form p").attr("class","trn");	
 
 function toEnglish(){
 $("#cc-checkout-submit-0").attr("Value","Next step");	
@@ -32,6 +40,25 @@ var local = {
 	"Versandkosten*<small></small>:":{en:"Shipping costs*:"},
 	" Gesamtsumme<small></small>:":{en:" Total:"},
 	"Anmerkung zur Bestellung":{en:"Customer order note"},
+	
+	"Rechnungs- und Lieferadresse":{en:"Billing and shipping address"},
+	"Rechnungsadresse":{en:"Billing address"},
+	"Anrede":{en:"Title"},
+	"Vorname <em class='j-checkout__required'>*</em>":{en:"First name *"},
+	"Nachname <em class='j-checkout__required'>*</em>":{en:"Last name *"},
+	"Firma":{en:"Company"},
+	"Straße, Nr. <em class='j-checkout__required'>*</em>":{en:"Street, No. *"},
+	"Adresszusatz":{en:"Address addition"},	
+	"Postleitzahl <em class='j-checkout__required'>*</em>":{en:"Zip Code"},	
+	"Stadt <em class='j-checkout__required'>*</em>":{en:"City *"},	
+	"Bundesland/Kanton":{en:"State"},	
+	"Telefon <em class='j-checkout__required'>*</em>":{en:"Telephone *"},		
+	"E-Mail <em class='j-checkout__required'>*</em>":{en:"E-Mail *"},	
+	"USt-IdNr.":{en:"VAT No."},	
+	"Bundesland/Kanton":{en:"State"},
+	"Land <em class='j-checkout__required'>*</em>":{en:"Country *"},	
+	" <em class='j-checkout__required'>*</em> Pflichtfeld":{en:"* Required"},
+	
 	"Herr":{en:"Mr"},
 	"Frau": {en:"Mrs"},
 	};	
