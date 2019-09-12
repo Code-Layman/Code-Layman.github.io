a7641e1ac484dbc4f89efc9a352caaab19277ce6
Code-Layman <54162370+Code-Layman@users.noreply.github.com>
2019-09-12 23:27:35 +0800

    Create english-searchbar.js

---
+++ english-searchbar.js
@@ -0,0 +1,18 @@
+var doofinder_script ='//cdn.doofinder.com/media/js/doofinder-fullscreen.6.latest.min.js';
+(function(d,t){var f=d.createElement(t),s=d.getElementsByTagName(t)[0];f.async=1;
+f.src=('https:'==location.protocol?'https:':'http:')+doofinder_script;
+f.setAttribute('charset','utf-8');
+s.parentNode.insertBefore(f,s)}(document,'script'));
+
+var dfFullscreenLayers = [{
+  "hashid": "2b547607f710abcd6b34e2c9af6d7917",
+  "zone": "eu1",
+  "display": {
+    "lang": "en",
+    "templateVars": {
+      "topbarLogo": "https://image.jimcdn.com/app/cms/image/transf/dimension=212x10000:format=jpg/path/sf287d20e30620f44/image/iebe030462fb5d6d0/version/1492098357/image.jpg",
+      "topbarLogoLink": "https://www.laborxing.com/english/"
+    }
+  },
+  "toggleInput": "body>div#cc-inner>div.jtpl-main.cc-content-parent>div.jtpl-section-main.content-options-box.cc-content-parent>div.jtpl-sidebar-content__container.cc-content-parent>aside.jtpl-sidebar>div.jtpl-sidebar__content.sidebar-options>div>div#cc-matrix-2982223798>div#cc-m-11191541098>form>input"
+}];
