c82f4eaf7d2086f27b2410f1b45976fc360daa08
Code-Layman <54162370+Code-Layman@users.noreply.github.com>
2019-09-12 23:20:51 +0800

    Create german-searchbar.js

---
+++ german-searchbar.js
@@ -0,0 +1,18 @@
+var doofinder_script ='//cdn.doofinder.com/media/js/doofinder-fullscreen.6.latest.min.js';
+(function(d,t){var f=d.createElement(t),s=d.getElementsByTagName(t)[0];f.async=1;
+f.src=('https:'==location.protocol?'https:':'http:')+doofinder_script;
+f.setAttribute('charset','utf-8');
+s.parentNode.insertBefore(f,s)}(document,'script'));
+
+var dfFullscreenLayers = [{
+  "toggleInput": "body>div#cc-inner>div.jtpl-main.cc-content-parent>div.jtpl-section-main.content-options-box.cc-content-parent>div.jtpl-sidebar-content__container.cc-content-parent>aside.jtpl-sidebar>div.jtpl-sidebar__content.sidebar-options>div>div#cc-matrix-2982223798>div#cc-m-11009725498>form>input",
+  "display": {
+    "templateVars": {
+      "topbarLogo": "https://image.jimcdn.com/app/cms/image/transf/dimension=212x10000:format=jpg/path/sf287d20e30620f44/image/iebe030462fb5d6d0/version/1492098357/image.jpg",
+      "topbarLogoLink": "www.laborxing.com"
+    },
+    "lang": "de"
+  },
+  "zone": "eu1",
+  "hashid": "09369e06b8ec8e56da1323e3bf749a61"
+}];
