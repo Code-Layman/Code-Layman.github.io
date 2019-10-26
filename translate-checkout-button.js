199dce9f3a19c7b5578e756157a590b577a0596e
Code-Layman <54162370+Code-Layman@users.noreply.github.com>
2019-10-26 09:50:11 +0800

    Update translate-checkout-button.js

--- translate-checkout-button.js
+++ translate-checkout-button.js
@@ -4,7 +4,7 @@ var webpage=window.location.href
 var reg = RegExp(/checkout/)
 if(webpage.match(reg)){ 
 $('div#cc-checkout-wrapper').before($('<select id="TranslateCheckout"><\/select>')); 
+$("#TranslateCheckout").append("<option value='en'>Checkout in English<\/option>");  
 $("#TranslateCheckout").append("<option value='de'>Checkout in Deutsch<\/option>");     
-$("#TranslateCheckout").append("<option value='en'>Checkout in English<\/option>");
 }
 });
